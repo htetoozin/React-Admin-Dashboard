2762bbc5985bc7697ae7018c7b73da7f1df0f3b2
Htet Oo Zin <htetoo.zin09@gmail.com>
2022-01-15 21:24:10 +0630

    Implement admin sidebar

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import "./TopBar.css";
+
+const TopBar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <div className="logo">
+            <span className="logo">React Admin</span>
+          </div>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt="profile"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
