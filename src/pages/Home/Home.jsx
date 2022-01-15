ea6a50d93ad41dd243df6797ac8c293f07e7b4c0
Htet Oo Zin <htetoo.zin09@gmail.com>
2022-01-15 22:32:03 +0630

    Add chart js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Chart from "../../components/Chart/Chart";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
